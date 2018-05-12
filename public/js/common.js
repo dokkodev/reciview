b14c09a37e1ef8bbba96e56c2aa03c15e863ddc2
thimtkfkd05 <thimtkfkd05@gmail.com>
2018-05-12 22:52:05 +0900

    update from goorm

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,14 @@
 $(function() {
 	$('#common-navbar').load('templates/common-navbar.html');
 });
+
+/****************************************
+ * Firebase SETUP
+ ****************************************/
+var config = {
+	apiKey: "AIzaSyAVgdgvjY50s8KJDa8u_q731_3jJPsuI44",
+	databaseURL: "https://cs374-2018-on-us-dp.firebaseio.com/",
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
